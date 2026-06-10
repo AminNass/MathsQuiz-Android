e272d53c8c002ae67ae5a899fedbbeecbc866a95
20055494 <aminelfassi2008@gmail.com>
2026-06-10 12:02:34 +0100

    Testing, because its too difficult for kids to do this.

--- mathCommon.py
+++ mathCommon.py
@@ -1,4 +1,21 @@
 import math
+from sympy import sympify
 
 def clamp(value, minimum, maximum):
     return max(minimum, min(value, maximum))
+
+def factors(num: int) -> list:
+
+    factors = set()
+
+    for i in range(1, int(math.sqrt(num))+1):
+        if num % i == 0:
+            factors.add(i)
+            factors.add(num // i)
+
+    sortedFactors = sorted(list(factors))
+    return sortedFactors
+
+def addAddition(expression, difficulty):
+    minNum = round(clamp(difficulty - 5, 1, 12))
+    maxNum = round(clamp(math.exp(2.1) * difficulty / 3, 2, 12))
--- mathsQuiz.py
+++ mathsQuiz.py
@@ -4,8 +4,6 @@ import mathCommon as mc
 import math
 from sympy import sympify
 
-infinity = float("inf")
-
 class Question:
 
     def __init__(self, question: str):
@@ -45,22 +43,75 @@ class QuestionManager:
         numNumbers = min(6, minNumNumber)
         print(f"{minNumNumber} - {maxNumNumber}")
 
-
-
         expression = ""
-        for num in range(numNumbers):
-            minNum = round(max(1, difficulty - 5))
-            maxNum = round(max(2, math.exp(2.1) * difficulty / 3))
-            number = random.randint(minNum, maxNum)
-            print(f"{minNum} - {maxNum}")
+        if type in ["+","-","*"]:
+            for num in range(numNumbers):
+                minNum = round(mc.clamp(difficulty - 5, 1, 12))
+                maxNum = round(mc.clamp(math.exp(2.1) * difficulty / 3, 2, 12))
+                number = random.randint(minNum, maxNum)
+                print(f"{minNum} - {maxNum}")
 
-            expression += f"{number} {type} "
+                expression += f"{number} {type} "
+        elif type == "/":
+            for num in range(numNumbers):
+                if num == 0:
+                    minNum = round(mc.clamp(difficulty - 5, 1, 12))
+                    maxNum = round(mc.clamp(math.exp(2.1) * difficulty / 3, 2, 12 ))
+                    number = random.randint(minNum, maxNum)
+                    print(f"{minNum} - {maxNum}")
+                    expression += f"{number} {type} "
+                    continue
+
+                currentAnswer = sympify(f"{expression}1")
+
+                factors = mc.factors(currentAnswer)
+                randomFactor = random.choice(factors)
+                expression += f"{randomFactor} {type} "
+
+
         expression = expression[:-2]
 
         question = Question(f"What is the sum of [{expression}]?")
         self.questions.append(question)
         return question
 
+    def genQuestion(self, type: str, difficulty: int):
+
+        if difficulty > 10 or difficulty < 1: raise ValueError("Difficulty must be a non-zero positive integer and no higher than 10.")
+
+        questionWord = ""
+        expression = ""
+        if type == "+":
+            randNumNumber = round(math.pow(1.05, difficulty) + 1)
+            numNumbers = min(6, randNumNumber)
+            questionWord = "What is the sum of"
+
+            for num in range(numNumbers):
+                minNum = round(mc.clamp(difficulty - 5, 1, 12))
+                maxNum = round(mc.clamp(math.exp(2.1) * difficulty / 3, 2, 12))
+                number = random.randint(minNum, maxNum)
+
+                expression += f"{number} {type} "
+
+        if type == "-":
+            randNumNumber = round(math.pow(1.05, difficulty) + 1)
+            numNumbers = min(6, randNumNumber)
+            questionWord = "What is the difference of"
+
+            for num in range(numNumbers):
+                if difficulty < 5:
+                    while
+                    minNum = round(mc.clamp(difficulty - 5, 1, 12))
+                    maxNum = round(mc.clamp(math.exp(2.1) * difficulty / 3, 2, 12))
+                    number = random.randint(minNum, maxNum)
+
+                    expression += f"{number} {type} "
+
+        expression = expression[:-2]
+
+        question = Question(f"{questionWord} [{expression}]?")
+        self.questions.append(question)
+        return question
 
 
 
@@ -72,6 +123,6 @@ print(p)
 print(int(q) + int(p))
 
 manager = QuestionManager(1)
-manager.generateQuestion("+", 18)
+manager.genQuestion("+", 12)
 
 print(manager.questions[0])
